ddedfb31852be8cec80d757094136d007c567c2f
Isaac Sebastián Guerra <isguerra@sudamericano.edu.ec>
2024-11-25 17:50:26 -0500

    Commit Update

--- spay-development/src/components/Header/Header.tsx
+++ spay-development/src/components/Header/Header.tsx
@@ -7,12 +7,12 @@ const Header: React.FC = () => {
     <div className={styles.header}>
       <div className={styles.topBar}>
         <nav className={styles.nav}>
-          <a href="#init">Init</a>
+          <a href="#init">Home</a>
           <a href="#spay"><img src={spayLogo} alt="Spay Logo" /></a>
-          <a href="#film">Film</a>
-          <a href="#about-us">About Us</a>
+          <a href="#film">Aplica ya</a>
+          {/* <a href="#about-us">About Us</a>
           <a href="#impact">Impact</a>
-          <a href="#contact">Contact</a>
+          <a href="#contact">Contact</a> */}
         </nav>
       </div>
     </div>
---
+++ spay-development/src/pages/Impact/Impact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Init.module.css';
+
+const Init: React.FC = () => {
+  return (
+    <section id="init" className={styles.init}>
+      <h1>Welcome to Init Section</h1>
+      <p>This is the beginning of the journey.</p>
+    </section>
+  );
+};
+
+export default Init;
