8a863fd112883cd9383931c2308b2d4ca752e7ba
Isaac Sebastián Guerra <isguerra@sudamericano.edu.ec>
2024-11-26 15:59:57 -0500

    Header path

--- spay-development/src/components/Header/Header.tsx
+++ spay-development/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import spayLogo from '../../assets/spay.svg'; 
+import spayLogo from '../../assets/spay.svg';
+
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
@@ -8,11 +9,10 @@ const Header: React.FC = () => {
       <div className={styles.topBar}>
         <nav className={styles.nav}>
           <a href="#init">Home</a>
-          <a href="#spay"><img src={spayLogo} alt="Spay Logo" /></a>
+          <a href="#spay">
+            <img src={spayLogo} alt="Spay Logo" />
+          </a>
           <a href="#film">Aplica ya</a>
-          {/* <a href="#about-us">About Us</a>
-          <a href="#impact">Impact</a>
-          <a href="#contact">Contact</a> */}
         </nav>
       </div>
     </div>
