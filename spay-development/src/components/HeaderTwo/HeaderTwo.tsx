0272eaaeb1410852239a63cf593deac599d45140
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-11-27 10:14:24 -0500

    Header changes svg

--- spay-development/src/components/HeaderTwo/HeaderTwo.tsx
+++ spay-development/src/components/HeaderTwo/HeaderTwo.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import spayLogo from '../../assets/spay.svg';
 
-import styles from './Header.module.css';
+import styles from './HeaderTwo.module.css';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      {/* SVG decorativo */}
-      <svg
-        className={styles.svg}
-        viewBox="0 0 1326 31"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-      >
-        <path
-          d="M0 1H357.584L386.975 30H934.127L962.538 1L1326 1"
-          className={styles.svgPath}
-        />
-      </svg>
-
-      {/* Contenido del navbar */}
       <nav className={styles.nav}>
         <a href="#init">Home</a>
         <a href="#spay">
